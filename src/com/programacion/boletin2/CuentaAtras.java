b042162981a705eec9c89529e798289f4603a936
Damian Nogueiras <damiannogueiras@gmail.com>
2020-10-25 10:43:16 +0100

    Cuenta atras

--- src/com/programacion/boletin2/CuentaAtras.java
+++ src/com/programacion/boletin2/CuentaAtras.java
@@ -1,4 +1,7 @@
 package com.programacion.boletin2;
 
 public class CuentaAtras {
+    public static void main(String[] args) {
+        System.out.println("Esto es la cuenta atras 3,2,1");
+    }
 }
