7ffea3c1ea22a6463c742bb46712ec857f997c18
sparrow <damian@danielcastelao.org>
2020-10-30 08:51:00 +0100

    Modificado ejercicio 3

--- src/com/programacion/boletin2/Ejercicio3.java
+++ src/com/programacion/boletin2/Ejercicio3.java
@@ -3,5 +3,6 @@ package com.programacion.boletin2;
 public class Ejercicio3 {
     public static void main(String[] args) {
         System.out.println("Hola ejercicio 3");
+        System.out.println("Modificacion");
     }
 }
