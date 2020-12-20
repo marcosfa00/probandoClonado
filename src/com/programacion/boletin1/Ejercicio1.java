b3fff392813f7e7ceafe891f11d4b7bf795e0b9e
Damián Nogueiras Meléndez <damian@danielcastelao.org>
2020-12-20 10:34:53 +0100

    update

--- src/com/programacion/boletin1/Ejercicio1.java
+++ src/com/programacion/boletin1/Ejercicio1.java
@@ -3,5 +3,6 @@ package com.programacion.boletin1;
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println("Hola Ejercicio 1 del Boletin 1");
+        // edición desde github
     }
 }
